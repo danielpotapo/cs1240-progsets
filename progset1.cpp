e99d6222ca3cbf9faea9e8be1151f3c2a1b013fb
DANIEL POTAPO <danielpotapo@college.harvard.edu>
2026-02-15 15:02:00 -0500

    general setup work

--- progset1.cpp
+++ progset1.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
+#include <vector>
+
+using namespace std;
+
+std::vector<std::vector<int>> graph;
+std::vector<std::vector<int>> output;
+
+
 
 int main() {
     std::cout << "hello!" << std::endl;
     return 0;
+}
+
+void prims(int size) {
+    return;
 }
