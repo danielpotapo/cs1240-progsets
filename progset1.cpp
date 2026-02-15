20e146cb6f681b49492b6410129e02f1b5cd1126
DANIEL POTAPO <danielpotapo@college.harvard.edu>
2026-02-15 15:36:08 -0500

    matrix generation

--- progset1.cpp
+++ progset1.cpp
@@ -1,15 +1,33 @@
 #include <iostream>
 #include <vector>
+#include <random>
 
 using namespace std;
 
-std::vector<std::vector<int>> graph;
-std::vector<std::vector<int>> output;
+vector<vector<int>> graph;
+vector<vector<int>> output;
 
 
+int main() {    
+    // generate random seed
+    srand(static_cast<unsigned int>(time(nullptr)));
 
-int main() {
-    std::cout << "hello!" << std::endl;
+    int max_size = 10;
+    int min_size = 1;
+    int graph_size = rand() % max_size + min_size;
+
+    // fill and printadjacency matrix
+    for (int i = 0; i < graph_size; i++) {
+        graph.push_back(*(new vector<int>));
+
+        for (int j = 0; j < graph_size; j++) {
+            graph[i].push_back(rand() % 2);
+            cout << graph[i][j] << "  ";    
+        }
+        
+        cout << endl;
+    }    
+
     return 0;
 }
 
