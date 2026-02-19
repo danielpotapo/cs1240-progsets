da931b47cc00c0aa3f5694129679862415df7c5e
DANIEL POTAPO <danielpotapo@college.harvard.edu>
2026-02-19 00:14:12 -0500

    prework done

--- randmst.cpp
+++ randmst.cpp
@@ -3,31 +3,41 @@
 #include <random>
 #include <cassert>
 #include <array>
+#include <algorithm>
 
 using namespace std;
 
 
-/*
-NOTES TO DEREK:
+// vertex definition
+struct Vert {
+    int rank;
+    int vertex;
+    Vert* parent;
+};
 
-I've implemented part of the prim's helper function. for now, it uses a less efficient
-algorithm for finding the minimum heap element, but we can optimze that later
-
-TODO: finish prims, main logic, optimize heap minimizing
-
-
-*/
+// edge definition
+struct Ed {
+    Vert* start;
+    Vert* end;
+    double weight;
+};
 
 vector<vector<double>> graph;         // representation of graph as an adjacency list
+vector<Vert> vertices;                // set of vertices for kruskal's algorithm
+vector<Ed> output;                    // set of edges for kruskal's algorithm
 
 
+//-------------------------------- graph generation ----------------------------------------//
 
-double generateWeight(uniform_real_distribution<double> dis, mt19937 engine, int dim) {
+double generateWeight(int dim) {
+    random_device rd;
+    mt19937 engine(rd());
+    uniform_real_distribution<double> dis(0, 1);
+
     // weights for uniform and hypercube uniformly random 
     if (dim == 0 || dim == 1) {
         return dis(engine);
     } 
-
     // weights for dim >= 2 are the euclidean distance between the points
     double inner = 0;
     for (int k = 0; k < dim; k++) {
@@ -37,7 +47,6 @@ double generateWeight(uniform_real_distribution<double> dis, mt19937 engine, int
 }
 
 void nDimensionalGraph(int points) {
-    
     // fill graph with points
     for (int i = 0; i < points; i++) {
         graph.push_back(*(new vector<double>));
@@ -50,16 +59,12 @@ void nDimensionalGraph(int points) {
         }
     }
 
-    // print graph (for debugging purposes)
-    for (int i = 0; i < points; i++) {
-        for (int j = 0; j < graph[i].size(); j++) {
-            cout << graph[i][j] << " ";
-        }
-        cout << endl;
-    }
-
-    cout << endl;
-    cout << endl;
+    // for (int i = 0; i < graph.size(); i++) {
+    //     for (int j = 0; j < graph[i].size(); j++) {
+    //         cout << graph[i][j] << " ";
+    //     }
+    //     cout << endl;
+    // }
 }
 
 void hyperCubeGraph(int points) {
@@ -81,6 +86,129 @@ void hyperCubeGraph(int points) {
         }
     }
 
+}
+
+//-------------------------------- kruskal's algorithm ----------------------------------------//
+
+void makeSet(Vert& x) {
+    vertices.push_back(x);
+    vertices.back().rank = 0;
+    vertices.back().parent = &vertices.back();
+}
+
+Vert* find(Vert& x) {
+    if (x.vertex != (*x.parent).vertex) {
+        x.parent = find(*x.parent);
+    }
+    return x.parent;    
+}
+
+void link(Vert& x, Vert& y) {
+    if (x.rank > y.rank) {
+        return link(y, x);
+    } else if (x.rank == y.rank) {
+        y.rank = y.rank + 1;
+    }
+    x.parent = &y;
+    return;
+}
+
+void createUnion(Vert& x, Vert& y) {
+    cout << "union between " << x.vertex << " and " << y.vertex << endl;
+    link(*find(x), *find(y));
+    return;
+}
+
+
+double kruskal(int dim) {
+    // output: X
+    // vertices: set of trees
+    vector<Ed> edges;  // set of edges
+
+    vertices.reserve(graph.size()); 
+
+    // make set u
+    for (int i = 0; i < graph.size(); i++) {
+        Vert add;
+        add.vertex = i;
+        makeSet(add);
+    }
+
+    // create set of edges and sort them
+    for (int i = 0; i < graph.size(); i++) {
+        for (int j = 0; j < graph[i].size(); j++) {
+            Ed add;
+            add.start = &vertices[i];    // vertices is initially sorted in increasing order
+            add.end = &vertices[j];
+            add.weight = generateWeight(dim);
+            edges.push_back(add);
+        }
+    }
+
+
+    sort(edges.begin(), edges.end(), [](const Ed& a, const Ed& b) {
+        return a.weight < b.weight;
+    });
+
+    
+    // main kruskal's algorithm logic
+    for (int i = 0; i < edges.size(); i++) {     
+        cout << i << endl;   
+        if ((*find(*edges[i].start)).vertex != (*find(*edges[i].end)).vertex) {
+            cout << "edges " << (*find(*edges[i].start)).vertex << " vs " << (*find(*edges[i].end)).vertex << endl;
+            output.push_back(edges[i]);
+            createUnion(*edges[i].start, *edges[i].end);
+        }
+    }
+
+    double total_weight = 0;
+    for (int i = 0; i < output.size(); i++) {
+        total_weight += output[i].weight;
+    }
+
+    cout << "MINIMUM SPANNING WEIGHTS" << endl;
+
+    for (int i = 0; i < output.size(); i++) {
+        cout << output[i].weight << endl;
+    }
+    cout << "NEXT TRIAL" << endl;
+
+    return total_weight;
+}
+
+//-------------------------------- main test logic sequence  -------------------------------------//
+
+int main(int argc, char* argv[]) {    
+    int numpoints = stoi(argv[2]);
+    int numtrials = stoi(argv[3]);
+    int dimension = stoi(argv[4]);
+
+    // remember to clear the graph afterwards
+
+    double result = 0;
+    for (int i = 0; i < numtrials; i++) {
+        if (dimension == 1) {
+            hyperCubeGraph(numpoints);
+        } else {
+            nDimensionalGraph(numpoints);
+        }
+        
+        result += kruskal(dimension);
+
+        graph.clear();
+        vertices.clear();
+        output.clear();
+    }
+
+    result = result/numtrials;
+
+    cout << "Final Result: " << result << endl;
+    
+    return 0;
+}
+
+
+/*
     // print graph (for debugging purposes)
     for (int i = 0; i < points; i++) {
         for (int j = 0; j < graph[i].size(); j++) {
@@ -90,98 +218,4 @@ void hyperCubeGraph(int points) {
     }
     cout << endl;
     cout << endl;
-}
-
-
-struct Vertex {
-    int rank;
-    int vertex;
-    Vertex* parent;
-};
-
-vector<array<double, 2>> heap;        // heap (for prims)
-vector<Vertex> vertices;              // vector of linked lists
-
-
-
-
-// inefficient minimum algorithm. returns index with minimum value in heap
-int findMin() {
-    double min = heap[0][1];
-    int minIndex = 0;
-    for (int i = 1; i < heap.size(); i++) {
-        if (heap[i][1] < min) {
-            min = heap[i][1];
-            minIndex = i;
-        }
-    }
-    return minIndex;
-}
-
-// E is union find set
-
-void makeSet() {
-    Vertex x;
-    x.rank = 0;
-    x.parent = &x;
-    vertices.push_back(x);
-}
-
-Vertex* find(Vertex x) {
-    if (x.vertex != (*x.parent).vertex) {
-        x.parent = find(*x.parent);
-    }
-    return x.parent;
-}
-
-void union(Vertex x, Vertex y) {
-
-}
-
-Vertex link(Vertex x, Vertex y) {
-    if (x.rank > y.rank) {
-        return link(y, x);
-    } else if (x.rank == y.rank) {
-        y.rank = y.rank + 1;
-    }
-    x.parent = &y;
-    return y;
-}
-
-
-
-int kruskal() {
-
-}
-
-
-int main(int argc, char* argv[]) {    
-    int numpoints = stoi(argv[2]);
-    int numtrials = stoi(argv[3]);
-    int dimension = stoi(argv[4]);
-
-    // remember to clear the graph afterwards
-
-    cout << "hypercube" << endl;
-    hyperCubeGraph(numpoints);
-    // cout << "ndimensional" << endl;
-    // nDimensionalGraph(numpoints);
-    
-
-    return 0;
-}
-
-
-/*
-We will need this later.
-
-generate random seed
-random_device rd;
-mt19937 engine(rd());
-uniform_real_distribution<double> dis(0, 1);
-
-
-
-
-
 */
