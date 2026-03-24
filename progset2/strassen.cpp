16be42ef5aad7ae57327c18ec8dfff8bdbca185f
DANIEL POTAPO <danielpotapo@college.harvard.edu>
2026-03-24 17:24:02 -0400

    full program

--- progset2/strassen.cpp
+++ progset2/strassen.cpp
@@ -4,22 +4,14 @@ NOTES TO DEREK:
 
 to run the program, run "make", then "./strassen <flag> <dimension> matrix.txt"
 
+PROGRAM COMMANDS:
+"./strassen 0 <dimension> matrix.txt": run strassen's on a matrix from matrix.txt of size dimension
 
-NOTE: I restructured some of the files; make sure you're in the progset2 directory
+"./strassen 1 <dimension> matrix.txt": average of 100 trials of strasses on a matrix of size dimension.
+note that the 0 index corresponds to the naive algorithm, and indices 1-10 are skipped b/c they take
+too long
 
-
-the first line of matrix.txt represents matrix A, second line represents matrix B
-
-
-i is row, j is column
-
-
-idea: add padding to bottom and right edges of matrix if it's an odd number (must prove that this works)
-
-
-
-
-ptr to an element of a vector: int* ptr = &vec.at(<index>);
+"./strassen 2 <dimension> matrix.txt": run a single trial of part 3
 */
 
 
@@ -42,9 +34,10 @@ using chrono::duration_cast;
 using chrono::duration;
 using chrono::milliseconds;
 
-vector<vector<int>> mat_a;
-vector<vector<int>> mat_b;
-vector<vector<int>> output;
+vector<long> mat_a;
+vector<long> mat_b;
+vector<long> mat_c;
+vector<long> mat_d;
 mt19937 rng; 
 
 
@@ -55,42 +48,63 @@ int threshold;
 
 
 
-void print(vector<vector<int>> mat) {
-    for (int i = 0; i < mat.size(); i++) {
-        for (int j = 0; j < mat.size(); j++) {
-            cout << " " << mat[i][j] << " ";
+int index(int i, int j, int stride) {
+    return i * stride + j;
+}
+
+long element(int i, int j, long* mat, int stride) {
+    return *(mat + index(i, j, stride));
+}
+
+int get_pow(int dimension) {
+    double exponent = 0;
+    while (pow(2, exponent) < dimension) {
+        exponent++;
+    }
+    return (int) pow(2, exponent);
+}
+
+
+
+
+void print(long* mat, int size) {
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            cout << " " << element(i, j, mat, size) << " ";
         }
         cout << endl;
     }
 }
 
 
-void print_diag(vector<vector<int>> mat) {
-    for (int i = 0; i < mat.size(); i++) {
-        cout << mat[i][i] << endl;
+void print_diag(long* mat, int size) {
+    for (int i = 0; i < size; i++) {
+        cout << element(i, i, mat, size) << endl;
     }
 }
 
-int add_diag(vector<vector<int>> mat) {
-    int sum = 0;
-    for (int i = 0; i < mat.size(); i++) {
-        sum += mat[i][i];
+long add_diag(long* mat, int size) {
+    long sum = 0;
+    for (int i = 0; i < size; i++) {
+        sum += element(i, i, mat, size);
     }
     return sum;
 }
 
 
-vector<vector<int>> generate(double p) {
-    vector<vector<int>> output;
+long* generate(double p) {
+    long* output = new long[1024 * 1024];
     uniform_real_distribution<double> dis(0, 1);
 
+    for (int i = 0; i < 1024 * 1024; i++) {
+        output[i] = 0;
+    }
+
     for (int i = 0; i < 1024; i++) {
-        output.push_back(vector<int>());
-        for (int j = 0; j < 1024; j++) {
+        for (int j = 0; j < i; j++) {
             if (dis(rng) <= p) {
-                output[i].push_back(1);
-            } else {
-                output[i].push_back(0);
+                output[index(i, j, 1024)] = 1;
+                output[index(j, i, 1024)] = 1;
             }
         }
     }
@@ -99,42 +113,41 @@ vector<vector<int>> generate(double p) {
 }
 
 
-vector<vector<int>> add(vector<vector<int>> one, vector<vector<int>> two, int size) {
-    vector<vector<int>> result = one;
+long* add(long* one, long* two, int size, int s1, int s2) {
+    long* result = new long[size * size];
 
     for (int i = 0; i < size; i++) {
         for (int j = 0; j < size; j++) {
-            result[i][j] = one[i][j] + two[i][j];
+            result[index(i, j, size)] = one[index(i, j, s1)] + two[index(i, j, s2)];
         }
     }
+
+    return result;
+}
+
+long* subtract(long* one, long* two, int size, int s1, int s2) {
+    long* result = new long[size * size];
+
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            result[index(i, j, size)] = one[index(i, j, s1)] - two[index(i, j, s2)];
+        }
+    }
+
     return result;
 }
 
 
-vector<vector<int>> subtract(vector<vector<int>> one, vector<vector<int>> two, int size) {
-    vector<vector<int>> result = one;
+long* naive_multiply(long* one, long* two, int size, int s1, int s2) {
+    long* result = new long[size * size];
 
-    for (int i = 0; i < size; i++) {
-        for (int j = 0; j < size; j++) {
-            result[i][j] = one[i][j] - two[i][j];
-        }
-    }
-    return result;
-}
-
-
-// start_col, start_row is the top left element
-
-vector<vector<int>> naive_multiply(vector<vector<int>> one, vector<vector<int>> two) {
-    vector<vector<int>> result = one;
-
-    for (int row = 0; row < one.size(); row++) {
-        for (int col = 0; col < one.size(); col++) {
-            int sum = 0;
-            for (int element = 0; element < one.size(); element++) {
-                sum += (one[row][element] * two[element][col]);
+    for (int row = 0; row < size; row++) {
+        for (int col = 0; col < size; col++) {
+            long sum = 0;
+            for (int e = 0; e < size; e++) {
+                sum += element(row, e, one, s1) * element(e, col, two, s2);
             }
-            result[row][col] = sum;   
+            result[index(row, col, size)] = sum;   
         }
     }
 
@@ -142,121 +155,124 @@ vector<vector<int>> naive_multiply(vector<vector<int>> one, vector<vector<int>> 
 }
 
 
-vector<vector<int>> multiply(vector<vector<int>> one, vector<vector<int>> two) {   // start is the top left
+long* multiply(long* one, long* two, int size, int s1, int s2) {   // start is the top left
     
-    if (one.size() <= threshold) {
-        return naive_multiply(one, two);
+    if (size <= threshold) {
+        return naive_multiply(one, two, size, s1, s2);
     }
     
     int prune = 0;
-    vector<vector<int>> result = one;
 
-    if (one.size() % 2 != 0 && one.size() != 1) {
-        prune = 1;
-        one.push_back(vector<int>());
-        two.push_back(vector<int>());
+    long* result = new long[size * size];
 
 
-        for (int i = 0; i < one[0].size(); i++) {
-            one[one.size() - 1].push_back(0);
-            two[two.size() - 1].push_back(0);
-        }
+    long* a = &one[index(0, 0, s1)];
+    long* b = &one[index(0, size/2, s1)];
+    long* c = &one[index(size/2, 0, s1)];
+    long* d = &one[index(size/2, size/2, s1)];
+    long* e = &two[index(0, 0, s2)];
+    long* f = &two[index(0, size/2, s2)];
+    long* g = &two[index(size/2, 0, s2)];
+    long* h = &two[index(size/2, size/2, s2)];
 
 
-        for (int i = 0; i < one.size(); i++) {
-            one[i].push_back(0);
-            two[i].push_back(0);
-        }
-    }
+    long* p1_1 = subtract(f, h, size/2, s2, s2);
+    long* p1 = multiply(a, p1_1, size/2, s1, size/2);
 
-    int size = one.size();
+    long* p2_1 = add(a, b, size/2, s1, s1);
+    long* p2 = multiply(p2_1, h, size/2, size/2, s2);
 
+    long* p3_1 = add(c, d, size/2, s1, s1);
+    long* p3 = multiply(p3_1, e, size/2, size/2, s2);
 
-     // base case
-    if (size == 1) {
-        result[0][0] *= two[0][0];
-        return result;
-    }
+    long* p4_1 = subtract(g, e, size/2, s2, s2);
+    long* p4 = multiply(d, p4_1, size/2, s1, size/2);
 
+    long* p5_1 = add(a, d, size/2, s1, s1);
+    long* p5_2 = add(e, h, size/2, s2, s2);
+    long* p5 = multiply(p5_1, p5_2, size/2, size/2, size/2);
 
-    vector<vector<int>> a;
-    vector<vector<int>> b;
-    vector<vector<int>> c;
-    vector<vector<int>> d;
-    vector<vector<int>> e;
-    vector<vector<int>> f;
-    vector<vector<int>> g;
-    vector<vector<int>> h;
+    long* p6_1 = subtract(b, d, size/2, s1, s1);
+    long* p6_2 = add(g, h, size/2, s2, s2);
+    long* p6 = multiply(p6_1, p6_2, size/2, size/2, size/2);
 
-    for (int i = 0; i < size/2; i++) {
-        a.push_back(vector<int>());
-        e.push_back(vector<int>());
-        for (int j = 0; j < size/2; j++) {
-            a[i].push_back(one[i][j]);
-            e[i].push_back(two[i][j]);
-        }
-    }
+    long* p7_1 = subtract(c, a, size/2, s1, s1);
+    long* p7_2 = add(e, f, size/2, s2, s2);
+    long* p7 = multiply(p7_1, p7_2, size/2, size/2, size/2);
 
 
-    for (int i = size/2; i < size; i++) {
-        c.push_back(vector<int>());
-        g.push_back(vector<int>());
-        for (int j = 0; j < size/2; j++) {
-            c[i - size/2].push_back(one[i][j]);
-            g[i - size/2].push_back(two[i][j]);
-        }
-    }
+    long* tl_1 = add(p4, p5, size/2, size/2, size/2);
+    long* tl_2 = subtract(p6, p2, size/2, size/2, size/2);
+    long* top_left = add(tl_1,tl_2 , size/2, size/2, size/2);
+
+    long* top_right = add(p1, p2, size/2, size/2, size/2);
+    long* bottom_left = add(p3, p4, size/2, size/2, size/2);
+
+    long* br_1 = add(p5, p7, size/2, size/2, size/2);
+    long* br_2 = subtract(p1, p3, size/2, size/2, size/2);
+    long* bottom_right = add(br_1, br_2, size/2, size/2, size/2);
 
 
-    for (int i = 0; i < size/2; i++) {
-        b.push_back(vector<int>());
-        f.push_back(vector<int>());
-        for (int j = size/2; j < size; j++) {
-            b[i].push_back(one[i][j]);
-            f[i].push_back(two[i][j]);
-        }
-    }
-
-
-    for (int i = size/2; i < size; i++) {
-        d.push_back(vector<int>());
-        h.push_back(vector<int>());
-        for (int j = size/2; j < size; j++) {
-            d[i - size/2].push_back(one[i][j]);
-            h[i - size/2].push_back(two[i][j]);
-        }
-    }
-
-
-    // this will have to change later for cases that aren't multiples of 2
-    vector<vector<int>> p1 = multiply(a, subtract(f, h, size/2));
-    vector<vector<int>> p2 = multiply(add(a, b, size/2), h);
-    vector<vector<int>> p3 = multiply(add(c, d, size/2), e);
-    vector<vector<int>> p4 = multiply(d, subtract(g, e, size/2));
-    vector<vector<int>> p5 = multiply(add(a, d, size/2), add(e, h, size/2));
-    vector<vector<int>> p6 = multiply(subtract(b, d, size/2), add(g, h, size/2));
-    vector<vector<int>> p7 = multiply(subtract(c, a, size/2), add(e, f, size/2));
-
-
-    vector<vector<int>> top_left = add(add(p4, p5, size/2), subtract(p6, p2, size/2), size/2);
-    vector<vector<int>> top_right = add(p1, p2, size/2);
-    vector<vector<int>> bottom_left = add(p3, p4, size/2);
-    vector<vector<int>> bottom_right = add(add(p5, p7, size/2), subtract(p1, p3, size/2), size/2);
-
-
-    for (int i = 0; i < size - prune; i++) {
-        for (int j = 0; j < size - prune; j++) {
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
             if (i < size/2 && j < size/2) {
-                result[i][j] = top_left[i][j];
+                result[index(i, j, size)] = element(i, j, top_left, size/2);
             } else if (i < size/2 && j >= size/2) {
-                result[i][j] = top_right[i][j - size/2];
+                result[index(i, j, size)] = element(i, j - size/2, top_right, size/2);
             } else if (i >= size/2 && j < size/2) {
-                result[i][j] = bottom_left[i - size/2][j];
+                result[index(i, j, size)] = element(i - size/2, j, bottom_left, size/2);
             } else {
-                result[i][j] = bottom_right[i - size/2][j - size/2];
+                result[index(i, j, size)] = element(i - size/2, j - size/2, bottom_right, size/2);
             }
         }
     }    
+
+    delete[] p1;
+    delete[] p1_1;
+    delete[] p2;
+    delete[] p2_1;
+    delete[] p3;
+    delete[] p3_1;
+    delete[] p4;
+    delete[] p4_1;
+    delete[] p5;
+    delete[] p5_1;
+    delete[] p5_2;
+    delete[] p6;
+    delete[] p6_1;
+    delete[] p6_2;
+    delete[] p7;
+    delete[] p7_1;
+    delete[] p7_2;
+    delete[] top_left;
+    delete[] tl_1;
+    delete[] tl_2;
+    delete[] top_right;
+    delete[] bottom_left;
+    delete[] bottom_right;
+    delete[] br_1;
+    delete[] br_2;
+
+    return result;
+}
+
+long* mat_mult(long* one, long* two, int size, int s1, int s2, int cut) {
+    long* multiplication = multiply(one, two, size, s1, s2);
+
+    if (cut == 0) {
+        return multiplication;
+    }
+
+    int new_size = size - cut;
+    long* result = new long[new_size * new_size];
+
+    for (int i = 0; i < new_size; i++) {
+        for (int j = 0; j < new_size; j++) {
+            result[index(i, j, new_size)] = element(i, j, multiplication, size);
+        }
+    }
+    delete[] multiplication;
+
     return result;
 }
 
@@ -269,14 +285,27 @@ int main(int argc, char* argv[]) {
 
     flag = stoi(argv[1]);
     dim = stoi(argv[2]);
+    int total_dim = get_pow(dim);
+    int to_cut = total_dim - dim;
+
+    
 
     // part 3
     if (flag == 2) {
+        threshold = 56;
         for (int i = 1; i <= 5; i++) {
-            vector<vector<int>> vec = generate(0.01 * i);
-            vector<vector<int>> result = multiply(vec, multiply(vec, vec));
+            long* vec = generate(0.01 * i);
+            dim = total_dim;
+            assert(dim == 1024);
+            
+            long* first_result = mat_mult(vec, vec, dim, dim, dim, 0);
+            long* result = mat_mult(vec, first_result, dim, dim, dim, 0);
 
-            cout << "for number " << i << ": " << add_diag(result)/6 << endl;
+            cout << "for value " << ((double) i) * 0.01 << ": " << add_diag(result, dim)/6 << endl;
+            
+            delete[] vec;
+            delete[] first_result;
+            delete[] result;
         }
 
         return 0;
@@ -285,39 +314,64 @@ int main(int argc, char* argv[]) {
 
     // run tests
     if (flag == 1) {
-        for (int j = 0; j < 2; j++) {
+        vector<double> counts;
+
+        for (int i = 0; i < dim/2; i++) {
+            counts.push_back(0);
+        }
+
+        for (int j = 1; j <= 100; j++) {
             int shortest_val = 1000;
             double shortest_time = 1000;
             mat_a.clear();
             mat_b.clear();
+            mat_c.clear();
+            mat_d.clear();
             uniform_real_distribution<double> dis(-9, 9);
 
-            for (int i = 0; i < dim; i++) {
-                mat_a.push_back(vector<int>());
-                mat_b.push_back(vector<int>());
-                for (int j = 0; j < dim; j++) {
-                    mat_a[i].push_back(dis(rng));
-                    mat_b[i].push_back(dis(rng));  
+            // fill matrices
+            for (int i = 0; i < total_dim ; i++) {
+                for (int j = 0; j < total_dim; j++) {
+                    if (i < dim && j < dim) {
+                        int num1 = dis(rng);
+                        int num2 = dis(rng);
+
+                        mat_a.push_back(num1);
+                        mat_b.push_back(num2);  
+                        mat_c.push_back(num1);
+                        mat_d.push_back(num2);
+                    } else {
+                        mat_c.push_back(0);
+                        mat_d.push_back(0);
+                    }
                 }
             }
 
+            // naive test
             auto previous = high_resolution_clock::now();
-            naive_multiply(mat_a, mat_b);
-            duration<double, milli> naive_time = high_resolution_clock::now() - previous;
-            cout << "naive time taken: " << naive_time.count() << endl;
 
+            long* naive = naive_multiply(mat_a.data(), mat_b.data(), dim, dim, dim);
+            duration<double, milli> naive_time = high_resolution_clock::now() - previous;
+            delete[] naive;
+            counts[0] += naive_time.count();
+
+            // general test
             for (int i = 10; i < dim/2; i++) {
                 threshold = i;
+
+                // calculate time
                 previous = high_resolution_clock::now();
-                multiply(mat_a, mat_b);
+                long* general = mat_mult(mat_c.data(), mat_d.data(), dim, dim, dim, to_cut);
                 duration<double, milli> time_taken = high_resolution_clock::now() - previous;
-                if (time_taken.count() < shortest_time) {
-                    shortest_time = time_taken.count();
-                    shortest_val = i;
-                }
-                cout << "time taken for " << i << ": " << time_taken.count() << endl;
+                delete[] general;
+
+                counts[i] += time_taken.count();
             }
-            cout << "SHORTEST VALUE: " << shortest_val << endl;
+            cout << "just finished trial " << j << endl;
+        }
+
+        for (int i = 0; i < counts.size(); i++) {
+            cout << "count of " << i << ": " << counts[i] / 100 << endl;
         }
         return 0;
     } 
@@ -333,41 +387,48 @@ int main(int argc, char* argv[]) {
         return 1;
     }    
 
+
     // fill vector representations of the arrays
-    for (int row = 0; row < dim; row++) {
-        mat_a.push_back(vector<int>());
-        for (int col = 0; col < dim; col++) {
-            getline(reader, line);
-            mat_a[row].push_back(stoi(line));  // ascii trick to convert char to int
+    for (int i = 0; i < total_dim; i++) {
+        for (int j = 0; j < total_dim; j++) {
+            if (j >= dim || i >= dim) {
+                mat_a.push_back(0);
+            } else {
+                getline(reader, line);
+                mat_a.push_back(stoi(line));  
+            }
         }
     }
 
-
-    for (int row = 0; row < dim; row++) {
-        mat_b.push_back(vector<int>());
-        for (int col = 0; col < dim; col++) {
-            getline(reader, line);
-            mat_b[row].push_back(stoi(line));
+    for (int i = 0; i < total_dim; i++) {
+        for (int j = 0; j < total_dim; j++) {
+            if (j >= dim || i >= dim) {
+                mat_b.push_back(0);
+            } else {
+                getline(reader, line);
+                mat_b.push_back(stoi(line));  
+            }
         }
     }
 
+    dim = total_dim;
     reader.close();
+
+    
 
     // print both matrices (bug checking)
     // cout << "MATRIX A" << endl;
-    // print(mat_a);
+    // print(mat_a.data(), dim);
 
 
     // cout << "MATRIX B" << endl;
-    // print(mat_b);
+    // print(mat_b.data(), dim);
 
 
     // cout << "OUTPUT" << endl;
 
-    threshold = 15;
-    print_diag(multiply(mat_a, mat_b));
-
-
+    threshold = 5;
+    print_diag(mat_mult(mat_a.data(), mat_b.data(), dim, dim, dim, to_cut), dim - to_cut);
 
     return 0;
 }
