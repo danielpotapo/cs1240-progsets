6c29a7def47bc9e5b6181b8e7cb1730519e37166
DANIEL POTAPO <danielpotapo@college.harvard.edu>
2026-04-23 19:02:24 -0400

    done

--- progset3/partition.cpp
+++ progset3/partition.cpp
@@ -10,16 +10,19 @@
 #include <cmath>    
 
 using namespace std;
+using chrono::high_resolution_clock;
+using chrono::duration_cast;
+using chrono::duration;
+using chrono::milliseconds;
+
+static mt19937 rng(random_device{}());
 vector<long long> values;
 
-
-// ./ partition flag algorithm inputfile
-
-
-void print_vector(vector<long long> v) {
+void print_vector(vector<double> v) {
     for (long long i = 0; i < v.size(); i++) {
-        cout << v[i] << " ";
+        cout << v[i] << "   ";
     }
+    cout << endl;
 }
 
 // return index of left element of heap (-1 if not in heap)
@@ -84,7 +87,7 @@ long long peek(vector<long long>& heap) {
     return heap[0];
 }
 
-long long kk(vector<long long>& heap) {
+long long kk(vector<long long> heap) {
     if (heap.size() == 2) {
         return abs(heap[1] - heap[0]);
     } else if (heap.size() == 1) {
@@ -137,13 +140,11 @@ long long compute_residue(vector<long long> v, vector<long long> signs) {
 }
 
 vector<long long> generate_random_sign(vector<long long> v) {
-    random_device rd;
-    mt19937 generate(rd());
     uniform_int_distribution<> distribution(0, 1);
     vector<long long> signs; 
 
     for (long long i = 0; i < v.size(); i++) {
-        long long rand = distribution(generate) * 2 - 1;
+        long long rand = distribution(rng) * 2 - 1;
         signs.push_back(rand);
     }
 
@@ -157,16 +158,14 @@ long long random_sign_residue(vector<long long> v) {
 }
 
 vector<long long> neighbor(vector<long long> v) {
-    random_device rd;
-    mt19937 generate(rd());
     uniform_int_distribution<> distribution(0, v.size() - 1);
     uniform_int_distribution<> smaller_distribution(0, v.size() - 2);
     uniform_int_distribution<> half(0, 1);
 
     vector<long long> result = v;
     
-    long long index_1 = distribution(generate);
-    long long index_2 = smaller_distribution(generate);
+    long long index_1 = distribution(rng);
+    long long index_2 = smaller_distribution(rng);
 
     if (index_2 >= index_1) {
         index_2 += 1;
@@ -174,7 +173,7 @@ vector<long long> neighbor(vector<long long> v) {
 
     result[index_1] = -1 * result[index_1];
 
-    if (half(generate) == 1) {
+    if (half(rng) == 1) {
         result[index_2] = -1 * result[index_2];
     }
 
@@ -182,15 +181,13 @@ vector<long long> neighbor(vector<long long> v) {
 }
 
 vector<long long> pre_neighbor(vector<long long> v, vector<long long> current_best_p) {
-    random_device rd;
-    mt19937 generate(rd());
     uniform_int_distribution<> distribution(0, v.size() - 1);
     uniform_int_distribution<> smaller_distribution(0, v.size() - 2);
 
     vector<long long> result = current_best_p;
 
-    long long element_index = distribution(generate);
-    long long new_bucket_index = smaller_distribution(generate);
+    long long element_index = distribution(rng);
+    long long new_bucket_index = smaller_distribution(rng);
 
     if (new_bucket_index >= current_best_p[element_index]) {
         new_bucket_index += 1;
@@ -201,7 +198,7 @@ vector<long long> pre_neighbor(vector<long long> v, vector<long long> current_be
     return result;
 }
 
-long long repeated_random(vector<long long> v) {
+long long repeated_random(vector<long long> v) {    
     long long minimum = random_sign_residue(v);
     for (long long i = 0; i < 25000; i++) {
         long long current = abs(random_sign_residue(v));
@@ -226,20 +223,21 @@ long long hill_climbing(vector<long long> v) {
     return compute_residue(v, current_best);
 }
 
-long long temperature(long long iter) {
-    return pow(10, 10 * pow(0.8, floor(iter / 300)));
+double temperature(long long iter) {
+    return pow(10, 10)* pow(0.8, floor(iter / 300));
 }
 
 long long simulated_annealing(vector<long long> v) {
     vector<long long> s = generate_random_sign(v);
     vector<long long> current_best = s;
+    uniform_real_distribution<> distribution(0, 1);
 
     for (long long iter = 0; iter < 25000; iter++) {
         vector<long long> s_prime = neighbor(s);
 
         if (compute_residue(v, s_prime) < compute_residue(v, s)) {
             s = s_prime;
-        } else if (exp(-1 * (compute_residue(v, s_prime) - compute_residue(v, s) / temperature(iter)))) {
+        } else if (distribution(rng) <= exp(-1 * ((compute_residue(v, s_prime) - compute_residue(v, s)) / temperature(iter)))) {
             s = s_prime;
         }
         if (compute_residue(v, s) < compute_residue(v, current_best)) {
@@ -252,18 +250,18 @@ long long simulated_annealing(vector<long long> v) {
 }
 
 long long pre_repeated_random(vector<long long> v) {
-    random_device rd;
-    mt19937 generate(rd());
     uniform_int_distribution<> distribution(0, v.size() - 1);
 
     vector<long long> current_best(v.size(), 0); 
 
     // ensure that current best is a valid heap for v
     for (long long i = 0; i < v.size(); i++) {
-        long long bucket_index = distribution(generate);
+        long long bucket_index = distribution(rng);
         current_best[bucket_index] += v[i];
     }
     build_heap(current_best);
+    long long kk_best = kk(current_best);
+
 
 
     for (long long j = 0; j < 25000; j++) {
@@ -272,14 +270,16 @@ long long pre_repeated_random(vector<long long> v) {
 
         // for each i, choose which bucket it belongs to randomly
         for (long long i = 0; i < v.size(); i++) {
-            long long bucket_index = distribution(generate);
+            long long bucket_index = distribution(rng);
             p.push_back(bucket_index);
             buckets[bucket_index] += v[i];
         }
 
         build_heap(buckets);
-        if (kk(buckets) < kk(current_best)) {
+        long long kk_buckets = kk(buckets);
+        if (kk_buckets < kk_best) {
             current_best = buckets;
+            kk_best = kk_buckets;
         }
     }
 
@@ -288,21 +288,21 @@ long long pre_repeated_random(vector<long long> v) {
 
 
 long long pre_hill_climbing(vector<long long> v) {
-    random_device rd;
-    mt19937 generate(rd());
     uniform_int_distribution<> distribution(0, v.size() - 1);
 
     vector<long long> current_best(v.size(), 0);       // array of sums resulting from best p
     vector<long long> current_best_p;                  // current best p array
+    
 
     // ensure that current best is a valid heap for v
     for (long long i = 0; i < v.size(); i++) {
-        long long bucket_index = distribution(generate);
+        long long bucket_index = distribution(rng);
         current_best_p.push_back(bucket_index);
         current_best[bucket_index] += v[i];
     }
 
     build_heap(current_best);
+    long long kk_best = kk(current_best);
 
     for (long long j = 0; j < 25000; j++) {
         vector<long long> p = pre_neighbor(v, current_best_p);
@@ -313,19 +313,21 @@ long long pre_hill_climbing(vector<long long> v) {
         }
 
         build_heap(buckets);
+        long long kk_buckets = kk(buckets);
 
-        if (kk(buckets) < kk(current_best)) {
+        if (kk_buckets < kk_best) {
             current_best = buckets;
+            current_best_p = p;
+            kk_best = kk_buckets;
         }
     }
 
-    return kk(current_best);
+    return kk_best;
 }
 
 long long pre_simulated_annealing(vector<long long> v) {
-    random_device rd;
-    mt19937 generate(rd());
     uniform_int_distribution<> distribution(0, v.size() - 1);
+    uniform_real_distribution<> prob_distribution(0, 1);
 
     vector<long long> current_best(v.size(), 0);       // array of sums resulting from best p
     vector<long long> current_best_p;                  // current best p array
@@ -335,7 +337,7 @@ long long pre_simulated_annealing(vector<long long> v) {
 
     // ensure that current best is a valid heap for v
     for (long long i = 0; i < v.size(); i++) {
-        long long bucket_index = distribution(generate);
+        long long bucket_index = distribution(rng);
         current_best_p.push_back(bucket_index);
         current_best[bucket_index] += v[i];
     }
@@ -345,7 +347,10 @@ long long pre_simulated_annealing(vector<long long> v) {
     build_heap(current_best);
     build_heap(current);
 
-    for (long long i = 0; i < 25000; i++) {
+    long long kk_best = kk(current_best);
+    long long kk_current = kk_best;
+
+    for (long long j = 0; j < 25000; j++) {
         vector<long long> p = pre_neighbor(v, current_p);
         vector<long long> buckets(v.size(), 0);  // candidate to beat current_best
 
@@ -354,21 +359,25 @@ long long pre_simulated_annealing(vector<long long> v) {
         }
 
         build_heap(buckets);
+        long long kk_buckets = kk(buckets);
 
-        if (kk(buckets) < kk(current_best)) {
+        if (kk_buckets < kk_current) {
             current_p = p;
             current = buckets;
-        } else if (exp(-1 * (kk(buckets) - kk(current) / temperature(i)))) {
+            kk_current = kk_buckets;
+        } else if (prob_distribution(rng) <= exp(-1 * ((kk_buckets - kk_current) / temperature(j)))) {
             current_p = p;
             current = buckets;
+            kk_current = kk_buckets;
         }
-        if (kk(current) < kk(current_best)) {
+        if (kk_current < kk_best) {
             current_best_p = current_p;
             current_best = current;
+            kk_best = kk_current;
         }
     }
 
-    return kk(current_best);
+    return kk_best;
     
 }
 
@@ -380,12 +389,85 @@ int main(int argc, char* argv[]) {
     }
 
     string line;
-    string flag = argv[1];
+    int flag = stoi(argv[1]);
     int algorithm = stoll(argv[2]);
     string file_name = argv[3];
     
     fstream reader(file_name);
     long long n_values;
+
+    random_device rd;
+    mt19937_64 generate(rd());
+    uniform_int_distribution<long long> distribution(1, pow(10, 12));
+
+    if (flag == 1) {
+        vector<double> times_0;
+        vector<double> times_1;
+        vector<double> times_2;
+        vector<double> times_3;
+        vector<double> times_11;
+        vector<double> times_12;
+        vector<double> times_13;
+
+        for (int i = 0; i < 50; i++) {
+            // fill values randomly
+            for (int j = 0; j < 100 ; j++) {
+                values.push_back(distribution(generate));
+            }
+
+            
+            build_heap(values);
+            
+            auto prev = high_resolution_clock::now();
+            cout << kk(values) << "    ";            // kk modifies by value
+            duration<double, milli> time_taken = high_resolution_clock::now() - prev;
+            times_0.push_back(time_taken.count());
+
+            prev = high_resolution_clock::now();
+            cout << repeated_random(values) << "    ";
+            time_taken = high_resolution_clock::now() - prev;
+            times_1.push_back(time_taken.count());
+
+            prev = high_resolution_clock::now();
+            cout << hill_climbing(values) << "    ";
+            time_taken = high_resolution_clock::now() - prev;
+            times_2.push_back(time_taken.count());
+
+            prev = high_resolution_clock::now();
+            cout << simulated_annealing(values) << "    ";
+            time_taken = high_resolution_clock::now() - prev;
+            times_3.push_back(time_taken.count());
+
+            prev = high_resolution_clock::now();
+            cout << pre_repeated_random(values) << "    ";
+            time_taken = high_resolution_clock::now() - prev;
+            times_11.push_back(time_taken.count());
+
+            prev = high_resolution_clock::now();
+            cout << pre_hill_climbing(values) << "    ";
+            time_taken = high_resolution_clock::now() - prev;
+            times_12.push_back(time_taken.count());
+
+            prev = high_resolution_clock::now();
+            cout << pre_simulated_annealing(values) << "    ";
+            time_taken = high_resolution_clock::now() - prev;
+            times_13.push_back(time_taken.count());
+            cout << endl;
+
+            values.clear();
+        }
+        cout << "DURATIONS:" << endl;
+        cout << endl;
+        print_vector(times_0);
+        print_vector(times_1);
+        print_vector(times_2);
+        print_vector(times_3);
+        print_vector(times_11);
+        print_vector(times_12);
+        print_vector(times_13);
+
+        return 0;
+    }
 
     if (!reader.is_open()) {
         cout << "invalid file" << endl;
@@ -401,6 +483,7 @@ int main(int argc, char* argv[]) {
     n_values = values.size();
 
     build_heap(values);
+
 
     if (algorithm == 0) {
         // cout << "KK: " << endl;
